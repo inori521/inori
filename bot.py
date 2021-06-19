d73f08b6dffd4b873db1d87df013b631ed48827e
inori521 <q0909115399@gmail.com>
2021-06-19 23:39:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='-')
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run('ODU1Nzc1MjA4MDA3MTM5MzI4.YM3YxQ.LFPApUwHNQvTgIs2sm44BcFIOC0')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+bot.run('ODU1Nzc1MjA4MDA3MTM5MzI4.YM3YxQ.LDhdhee6zG2V81M1Tlu6BX6F4Ew')
